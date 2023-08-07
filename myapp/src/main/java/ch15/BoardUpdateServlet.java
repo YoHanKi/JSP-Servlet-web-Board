ea877c9f12100c54cf4c7be39f50733ccf5dc901
JOHN <JOHN@211.206.244.214>
2023-08-07 22:24:48 +0900

    JSP/Servlet/MySQL을 이용한 게시판 만들기

--- myapp/src/main/java/ch15/BoardUpdateServlet.java
+++ myapp/src/main/java/ch15/BoardUpdateServlet.java
@@ -1,11 +1,14 @@
 package ch15;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/ch15/boardUpdate")
 public class BoardUpdateServlet extends HttpServlet {
